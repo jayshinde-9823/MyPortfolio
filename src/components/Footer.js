c9ca09f64a322e27b00421beb18446f517a5462c
Shinde Jay Jitendra <146534752+jayshinde-9823@users.noreply.github.com>
2025-09-03 14:41:14 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,1 +1,12 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <footer className="bg-gray-900 py-6 text-center text-gray-400 border-t border-gray-700">
+      <p>© {new Date().getFullYear()} Jay Shinde. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
 
