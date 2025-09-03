8f97886433e71b28eec0529e7314d786f5121d66
Shinde Jay Jitendra <146534752+jayshinde-9823@users.noreply.github.com>
2025-09-03 14:44:07 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  darkMode: 'class', // 👈 enable dark mode toggle
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
     "./public/index.html",
